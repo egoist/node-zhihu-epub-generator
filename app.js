efb01fd49b8a523bbe4585b4a59c395de8ad4479
0x142857 <0x142857@gmail.com>
2015-01-11 16:29:49 +0800

    make it non-block

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
 var fs = require('fs');
 var rest = require('restler');
-var forEach = require('async-foreach').forEach;
 var cheerio = require('cheerio');
-
-
+var EventProxy = require('eventproxy');
+var ep = new EventProxy();
 
 var url = [
-  'http://www.zhihu.com/question/27478526',
-  'http://www.zhihu.com/question/27441042',
-  'http://www.zhihu.com/question/23021337',
-  'http://www.zhihu.com/question/26758179',
-  'http://www.zhihu.com/question/27480171',
-  'http://www.zhihu.com/question/19858451',
-  'http://www.zhihu.com/question/27448909'
+'http://www.zhihu.com/question/26968485',
+'http://www.zhihu.com/question/27441042',
+'http://www.zhihu.com/question/23021337',
+'http://www.zhihu.com/question/26758179',
+'http://www.zhihu.com/question/27442419',
+'http://www.zhihu.com/question/19858451',
+'http://www.zhihu.com/question/25015549'
 ];
 var output = '';
+var index;
 
 
-forEach(url, function(post, index, arr){
-  var answers = '';
-  var order = index + 1;
-  console.log('fetching no.'+order);
+var order = 1;
+
+load(0);
+
+function load(index) {
   rest.get(url[index]).on('complete', function(result) {
+    console.log('fetching no.' + order);
+    order++;
+    $ = cheerio.load(result, {
+      normalizeWhitespace: false,
+      xmlMode: false,
+      decodeEntities: false
+    });
 
-    $ = cheerio.load(result);
+    var answers = '';
 
-    $('img').each(function(){
-      $(this).attr('src',$(this).data('actualsrc'));
-      $(this).css('margin','20px 0')
+    $('img').each(function() {
+      $(this).attr('src', $(this).data('actualsrc'));
+      $(this).css('margin', '20px 0')
     })
+
+    $('noscript').remove();
 
     $('.toggle-expand').remove();
 
-    $('.zm-item-answer').each(function(i,elem){
+    $('.zm-item-answer').each(function(i, elem) {
 
-      if(i < 10){
+      if (i < 7) {
         var user = $(this).find('.zm-item-answer-author-wrap').text();
-        user = '<div style="font-weight:700;margin:10px 0;">▉ '+user+'</div>';
+        user = '<div style="font-weight:700;margin:10px 0;">▉ ' + user + '</div>';
         var answer = $(this).find('.zm-editable-content').html();
+        if (!answer) answer = '>这个回复被知乎和谐了';
         answers += user + answer + '<hr>';
       }
 
@@ -49,23 +60,42 @@ forEach(url, function(post, index, arr){
 
 
     var title = $('.zm-item-title').html();
-    title = '<h2>'+title+'</h2>';
+    if (!title) title = url[index];
+    console.log(title)
+    title = '<h2>' + title + '</h2>';
+
     var content = $('#zh-question-detail').find('.zm-editable-content').html();
+
     content += '<hr>';
 
     output += '<div>' + title + content + answers + '</div>';
 
-    if(index == url.length - 1){
 
-        fs.writeFile('output.html', output, function (err) {
-          if (err) throw err;
-          console.log('It\'s saved!');
-        });
+    index++;
+    if (index < url.length)
+      ep.emit('next', index);
 
 
-    }else{
-      output += append;
-    }
+      if (index == url.length) {
+        ep.emit('save', output);
 
-  })
+
+      } else {
+        output += append;
+      }
+    })
+}
+
+
+
+ep.tail('next', function(next) {
+  load(next);
 })
+
+
+ep.tail('save', function(save) {
+  fs.writeFile('output.html', save, function(err) {
+    if (err) throw err;
+    console.log('It\'s saved as html! now exporting to epub');
+  });
+});
